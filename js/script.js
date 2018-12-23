7730712980c5c552de7c38255600809c53c33319
MagdaMB <magda.m.badura@gmail.com>
2018-12-23 21:50:27 +0100

    ogolne rozwiazanie

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ document.body.addEventListener("mousemove", (event) => {
 
     document.querySelector("h1").textContent = `Moja pozycja to: ${event.clientX}, ${event.clientY}`;
 
-    document.body.style.backgroundColor = `#${event.clientX}${event.clientY}`;
+    // document.body.style.backgroundColor = `#${event.clientX}${event.clientY}`;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    document.body.style.backgroundColor = `rgb(${red},${green},${blue}`;
 
 })
