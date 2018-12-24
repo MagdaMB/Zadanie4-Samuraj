534458c82878684f73b47fca16231a6560821b79
MagdaMB <magda.m.badura@gmail.com>
2018-12-24 20:18:53 +0100

    zmiana koloru tla na klik

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-document.body.addEventListener("mousemove", (event) => {
-
-    document.querySelector("h1").textContent = `Moja pozycja to: ${event.clientX}, ${event.clientY}`;
-
-    // document.body.style.backgroundColor = `#${event.clientX}${event.clientY}`;
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    const red = Math.floor(Math.random() * 255);
-    const green = Math.floor(Math.random() * 255);
-    const blue = Math.floor(Math.random() * 255);
-
-    document.body.style.backgroundColor = `rgb(${red},${green},${blue}`;
-
+document.body.addEventListener("click", (event) => {
+    if ((event.clientX % 2 === 0) && (event.clientY % 2 === 0)) {
+        document.body.style.backgroundColor = "red";
+        document.querySelector("h1").textContent = `Moje współrzędne to: ${event.clientX}, ${event.clientY}`;
+    } else if ((event.clientX % 2 != 0) && (event.clientY % 2 != 0)) {
+        document.body.style.backgroundColor = "blue";
+        document.querySelector("h1").textContent = `Moje współrzędne to: ${event.clientX}, ${event.clientY}`;
+    } else {
+        document.body.style.backgroundColor = "green";
+        document.querySelector("h1").textContent = `Moje współrzędne to: ${event.clientX}, ${event.clientY}`;
+    }
 })
