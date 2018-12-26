33a2bc84ba8a370190a19e4544d2a9d3ea80331a
MagdaMB <magda.m.badura@gmail.com>
2018-12-26 11:10:33 +0100

    dwie wersje switch/if

--- js/script.js
+++ js/script.js
@@ -1,15 +1,40 @@
-document.body.addEventListener("mousemove", (event) => {
+let red = 100;
+let green = 100;
+let blue = 100;
 
-    document.querySelector("h1").textContent = `Moja pozycja to: ${event.clientX}, ${event.clientY}`;
+document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
 
-    // document.body.style.backgroundColor = `#${event.clientX}${event.clientY}`;
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+const changeColor = (e) => {
+    //instrukcja if
+    // if (e.keyCode === 38) {
+    //     red -= 5;
+    //     green -= 5;
+    //     blue -= 5;
+    // } else if (e.keyCode === 40) {
+    //     red += 5;
+    //     green += 5;
+    //     blue += 5;
+    // } else {
+    //     alert("Naciśnij strzałkę w dół lub w górę!!!");
+    // }
 
-    const red = Math.floor(Math.random() * 255);
-    const green = Math.floor(Math.random() * 255);
-    const blue = Math.floor(Math.random() * 255);
+    //instrukcja switch
+    switch (e.keyCode) {
+        case 38:
+            red -= 5;
+            green -= 5;
+            blue -= 5;
+            break;
+        case 40:
+            red += 5;
+            green += 5;
+            blue += 5;
+            break;
+        default:
+            alert("Naciśnij strzałkę w dół lub w górę!!!");
+    }
 
-    document.body.style.backgroundColor = `rgb(${red},${green},${blue}`;
+    document.body.style.backgroundColor = `rgb(${red},${green},${blue})`;
+}
 
-})
+window.addEventListener('keydown', changeColor);
